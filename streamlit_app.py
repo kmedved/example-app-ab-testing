0c8777f928f35d3a466e63429a996f5fc7cc8eca
kmedved <bibigon@gmail.com>
2023-03-14 15:36:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,10 @@ def update_elo_ratings(rating1, rating2, result, k_factor=32):
     
     return new_rating1, new_rating2
 
+def select_new_players(df):
+    player1, player2 = random.sample(df['Player'].tolist(), 2)
+    return player1, player2
+
 def main():
     st.title("NBA Players Elo Comparison")
 
@@ -22,23 +26,28 @@ def main():
 
     df = pd.DataFrame(data)
 
-    player1, player2 = random.sample(df['Player'].tolist(), 2)
+    session_state = st.session_state
 
-    st.write(f"**Player 1:** {player1}")
-    st.write(f"**Player 2:** {player2}")
+    if "player1" not in session_state or "player2" not in session_state:
+        session_state.player1, session_state.player2 = select_new_players(df)
 
-    choice = st.radio("Who is the better player?", (player1, player2))
+    st.write(f"**Player 1:** {session_state.player1}")
+    st.write(f"**Player 2:** {session_state.player2}")
+
+    choice = st.radio("Who is the better player?", (session_state.player1, session_state.player2))
 
     if st.button('Submit'):
-        result = 1 if choice == player1 else 0
-        p1_elo, p2_elo = df[df['Player'] == player1]['Elo'].values[0], df[df['Player'] == player2]['Elo'].values[0]
+        result = 1 if choice == session_state.player1 else 0
+        p1_elo, p2_elo = df[df['Player'] == session_state.player1]['Elo'].values[0], df[df['Player'] == session_state.player2]['Elo'].values[0]
         new_p1_elo, new_p2_elo = update_elo_ratings(p1_elo, p2_elo, result)
 
-        st.write(f"**{player1}** Elo: {p1_elo:.0f} -> {new_p1_elo:.0f}")
-        st.write(f"**{player2}** Elo: {p2_elo:.0f} -> {new_p2_elo:.0f}")
+        st.write(f"**{session_state.player1}** Elo: {p1_elo:.0f} -> {new_p1_elo:.0f}")
+        st.write(f"**{session_state.player2}** Elo: {p2_elo:.0f} -> {new_p2_elo:.0f}")
     
-        winner = player1 if result == 1 else player2
+        winner = session_state.player1 if result == 1 else session_state.player2
         st.write(f"**{winner}** is the better player according to the Elo ratings.")
+        
+        session_state.player1, session_state.player2 = select_new_players(df)
 
 if __name__ == "__main__":
     main()
